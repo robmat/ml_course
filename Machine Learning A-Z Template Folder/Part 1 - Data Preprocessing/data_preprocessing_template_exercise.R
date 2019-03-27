709963d2d814e3a278d4e689ed99b684168e97e1
Robert Batorowski (PGS Software) <rbatorowski@pgs-soft.com>
2019-03-27 14:51:42 +0100

    Part 2

--- Machine Learning A-Z Template Folder/Part 1 - Data Preprocessing/data_preprocessing_template_exercise.R
+++ Machine Learning A-Z Template Folder/Part 1 - Data Preprocessing/data_preprocessing_template_exercise.R
@@ -1,4 +1,5 @@
 dataset = read.csv('Data.csv')
+
 dataset$Age = ifelse(test = is.na(dataset$Age),
                      yes = ave(dataset$Age, FUN = function(x) mean(x, na.rm = TRUE)), 
                      no = dataset$Age)
@@ -6,3 +7,21 @@ dataset$Age = ifelse(test = is.na(dataset$Age),
 dataset$Salary = ifelse(test = is.na(dataset$Salary),
                         yes = ave(dataset$Salary, FUN = function(x) mean(x, na.rm = TRUE)), 
                         no = dataset$Salary)
+
+dataset$Country = factor(x = dataset$Country,
+                         levels = c('France', 'Spain', 'Germany'),
+                         labels = c(1, 2, 3))
+dataset$Purchased = factor(x = dataset$Purchased,
+                           levels = c('No', 'Yes'),
+                           labels = c(0, 1))
+
+# install.packages('caTools')
+# library(caTools)
+set.seed(seed = 123)
+split = sample.split(Y = dataset$Purchased,
+                     SplitRatio = 0.8)
+
+trianing_set = subset(x = dataset,
+                      subset = split == TRUE)
+test_set = subset(x = dataset,
+                      subset = split == FALSE)
--- Machine Learning A-Z Template Folder/Part 1 - Data Preprocessing/data_preprocessing_template_exercise.py
+++ Machine Learning A-Z Template Folder/Part 1 - Data Preprocessing/data_preprocessing_template_exercise.py
@@ -1,7 +1,6 @@
 # -*- coding: utf-8 -*-
 """
 Created on Tue Mar 26 21:03:30 2019
-
 @author: Bator
 """
 
@@ -20,3 +19,15 @@ from sklearn.preprocessing import Imputer
 imputer = Imputer(missing_values='NaN', strategy='mean', axis=0, verbose=5)
 imputer = imputer.fit(X[:, 1:3])
 X[:, 1:3] = imputer.transform(X[:, 1:3])
+
+from sklearn.preprocessing import LabelEncoder, OneHotEncoder
+# labelencode_x = LabelEncoder()
+# X[:, 0] = labelencode_x.fit_transform(X[:, 0])
+oneHotEncoder_x = OneHotEncoder(categorical_features=[0])
+X = oneHotEncoder_x.fit_transform(X).toarray()
+
+labelencode_y = LabelEncoder()
+Y = labelencode_y.fit_transform(Y)
+
+from sklearn.model_selection import train_test_split 
+X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size = 0.2, random_state = 0)
