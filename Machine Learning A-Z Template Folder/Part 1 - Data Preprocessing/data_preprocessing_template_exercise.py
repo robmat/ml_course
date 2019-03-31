ac4bf187ac2c1d160f7d610121b364075f1d2f05
Robert Batorowski (PGS Software) <rbatorowski@pgs-soft.com>
2019-03-31 20:51:42 +0200

    'progress'

--- Machine Learning A-Z Template Folder/Part 1 - Data Preprocessing/data_preprocessing_template_exercise.py
+++ Machine Learning A-Z Template Folder/Part 1 - Data Preprocessing/data_preprocessing_template_exercise.py
@@ -11,23 +11,31 @@ import pandas as pd
 dataset = pd.read_csv('Data.csv')
 
 X = dataset.iloc[:, :-1].values
-
 Y = dataset.iloc[:, 3].values
 
+"""
 from sklearn.preprocessing import Imputer
-
 imputer = Imputer(missing_values='NaN', strategy='mean', axis=0, verbose=5)
 imputer = imputer.fit(X[:, 1:3])
 X[:, 1:3] = imputer.transform(X[:, 1:3])
+"""
 
+"""
 from sklearn.preprocessing import LabelEncoder, OneHotEncoder
-# labelencode_x = LabelEncoder()
-# X[:, 0] = labelencode_x.fit_transform(X[:, 0])
+labelencode_x = LabelEncoder()
+X[:, 0] = labelencode_x.fit_transform(X[:, 0])
 oneHotEncoder_x = OneHotEncoder(categorical_features=[0])
 X = oneHotEncoder_x.fit_transform(X).toarray()
-
 labelencode_y = LabelEncoder()
 Y = labelencode_y.fit_transform(Y)
+"""
 
 from sklearn.model_selection import train_test_split 
 X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size = 0.2, random_state = 0)
+
+"""
+from sklearn.preprocessing import StandardScaler
+sc_X = StandardScaler()
+X_train = sc_X.fit_transform(X_train)
+X_test = sc_X.transform(X_test)
+"""
