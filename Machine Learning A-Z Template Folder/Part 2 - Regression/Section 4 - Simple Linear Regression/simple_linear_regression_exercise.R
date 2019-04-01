7d324b7a1dc274af55e9d03e6922a05369bc93d8
Robert Batorowski (PGS Software) <rbatorowski@pgs-soft.com>
2019-04-01 08:02:36 +0200

    'progress'

---
+++ Machine Learning A-Z Template Folder/Part 2 - Regression/Section 4 - Simple Linear Regression/simple_linear_regression_exercise.R
@@ -0,0 +1,38 @@
+# Data Preprocessing Template
+
+# Importing the dataset
+dataset = read.csv('Salary_data.csv')
+
+# Splitting the dataset into the Training set and Test set
+# install.packages('caTools')
+library(caTools)
+set.seed(123)
+split = sample.split(dataset$Salary, SplitRatio = 2/3)
+training_set = subset(dataset, split == TRUE)
+test_set = subset(dataset, split == FALSE)
+
+# Feature Scaling
+# training_set = scale(training_set)
+# test_set = scale(test_set)
+y_pred = predict(regressor, newdata = test_set)
+y_pred_train = predict(regressor, newdata = training_set)
+
+# install.packages('ggplot2')
+library(ggplot2)
+ggplot() +
+  geom_point(aes(x = training_set$YearsExperience, y = training_set$Salary),
+             colour = 'red') +
+  geom_line(aes(x = training_set$YearsExperience, y = y_pred_train),
+            colour = 'blue') +
+  ggtitle('Salary vs experience (training set)') +
+  xlab('Years of experience') +
+  ylab('Salary')
+
+ggplot() +
+  geom_point(aes(x = test_set$YearsExperience, y = test_set$Salary),
+             colour = 'red') +
+  geom_line(aes(x = training_set$YearsExperience, y = y_pred_train),
+            colour = 'blue') +
+  ggtitle('Salary vs experience (test set)') +
+  xlab('Years of experience') +
+  ylab('Salary')
